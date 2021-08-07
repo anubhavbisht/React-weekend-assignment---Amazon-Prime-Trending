d31970894c7717b64d7a012246dabd678ec4f08b
Anubhav Bisht <40894333+anubhavbisht@users.noreply.github.com>
2021-08-07 21:12:51 +0530

    dasd

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,57 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
 
-const App = () => {
+import "../styles/App.css";
+
+const App = (props) => {
+  console.log(props.slides);
+
+  const [currSlides, setCurrSlides] = useState(0);
+
+  const clickPrev = () => {
+    currSlides > 0 && setCurrSlides(currSlides - 1);
+  };
+
+  const clickNext = () => {
+    currSlides < props.slides.length - 1 && setCurrSlides(currSlides + 1);
+  };
+
+  const clickRestart = () => {
+    setCurrSlides(0);
+  };
+
   return (
-    <></>
-  )
-}
+    <div className="App">
+      <div className="carousel">
+        <h1 data-testid="title">{props.slides[currSlides].title}</h1>
 
+        <p data-testid="text">{props.slides[currSlides].text}</p>
+      </div>
+
+      <button
+        onClick={clickPrev}
+        disabled={currSlides === 0 ? true : false}
+        data-testid="button-prev"
+      >
+        Prev
+      </button>
+
+      <button
+        onClick={clickNext}
+        disabled={currSlides === props.slides.length - 1 ? true : false}
+        data-testid="button-next"
+      >
+        Next
+      </button>
+
+      <button
+        onClick={clickRestart}
+        disabled={currSlides === 0 ? true : false}
+        data-testid="button-restart"
+      >
+        Restart
+      </button>
+    </div>
+  );
+};
 
 export default App;
